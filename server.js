126458cab6a415efbe70f94554e230fa086912cc
elrobady-code <elrobady@gmail.com>
2026-09-20 14:26:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,10 @@ const io = new Server(server);
 
 app.use(express.static(__dirname));
 
-let salas = {}; // Almacena el estado de cada sala
+let salas = {};
 
 io.on('connection', (socket) => {
-    console.log('Usuario conectado:', socket.id);
-
-    // Crear sala
+    // Crear sala con temática
     socket.on('crearSala', (data) => {
         const { codigo, listaPreguntas } = data;
         
@@ -43,26 +41,23 @@ io.on('connection', (socket) => {
             socket.join(salaUpper);
             socket.emit('unidoExito', salaUpper);
             
-            // Actualizar lista en pantalla del Host
             io.to(salas[salaUpper].host).emit('actualizarListaJugadores', salas[salaUpper].jugadores);
         } else {
             socket.emit('errorUnirse', 'Esta sala no existe o el código es incorrecto.');
         }
     });
 
-    // Anfitrión inicia la siguiente pregunta
+    // Anfitrión inicia o pasa a la siguiente pregunta
     socket.on('siguientePregunta', (codigo) => {
         const sala = salas[codigo];
         if (sala && socket.id === sala.host) {
-            // Limpiar cronómetro anterior si existía
             if (sala.intervaloPregunta) clearInterval(sala.intervaloPregunta);
 
             if (sala.indicePregunta < sala.preguntas.length) {
                 const preguntaActual = sala.preguntas[sala.indicePregunta];
                 sala.tiempoRestante = 15;
-                sala.respuestasEstaRonda = {}; // Reiniciar respuestas de los jugadores
+                sala.respuestasEstaRonda = {};
 
-                // Enviar la pregunta a TODOS (Host y celulares) sin revelar la correcta
                 io.to(codigo).emit('lanzarPregunta', {
                     pregunta: preguntaActual.pregunta,
                     opciones: preguntaActual.opciones,
@@ -71,46 +66,43 @@ io.on('connection', (socket) => {
                     tiempo: sala.tiempoRestante
                 });
 
-                // Iniciar cronómetro de 15 segundos en el servidor
                 sala.intervaloPregunta = setInterval(() => {
                     sala.tiempoRestante--;
                     io.to(codigo).emit('actualizarTiempo', sala.tiempoRestante);
 
                     if (sala.tiempoRestante <= 0) {
                         clearInterval(sala.intervaloPregunta);
-                        // Revelar respuesta correcta a todos
                         io.to(codigo).emit('finTiempo', {
                             correcta: preguntaActual.correcta,
                             puntajes: sala.jugadores
                         });
-                        sala.indicePregunta++;
+                        sala.indicePregunta++; // Se incrementa SOLO al finalizar la pregunta
                     }
                 }, 1000);
 
             } else {
-                // Fin del juego: Enviar podio ordenado por puntaje
                 const ranking = Object.values(sala.jugadores).sort((a, b) => b.puntaje - a.puntaje);
                 io.to(codigo).emit('juegoTerminado', ranking);
             }
         }
     });
 
-    // Recibir respuesta del celular del jugador
+    // Recibir respuesta del jugador
     socket.on('enviarRespuesta', (data) => {
         const { codigo, opcionIndex, tiempoRestante } = data;
         const sala = salas[codigo];
         
         if (sala && sala.jugadores[socket.id] && !sala.respuestasEstaRonda[socket.id]) {
             sala.respuestasEstaRonda[socket.id] = true;
-            const preguntaActual = sala.preguntas[sala.indicePregunta - 1]; // La activa
+            const preguntaActual = sala.preguntas[sala.indicePregunta];
 
-            if (opcionIndex === preguntaActual.correcta) {
-                // Puntos basados en velocidad (máx 1000 puntos + extra por tiempo restante)
+            const esCorrecta = (opcionIndex === preguntaActual.correcta);
+            if (esCorrecta) {
                 const puntosGanados = 500 + (tiempoRestante * 33);
                 sala.jugadores[socket.id].puntaje += Math.round(puntosGanados);
             }
 
-            // Notificar al host cuántos van respondiendo
+            socket.emit('resultadoIndividual', esCorrecta);
             io.to(sala.host).emit('actualizarProgresoRespuestas', Object.keys(sala.respuestasEstaRonda).length);
         }
     });
